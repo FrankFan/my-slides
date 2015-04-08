34d98c087d0ed639dd373d8adb55cf66b87dee28
FrankFan <fanyong@gmail.com>
2015-04-08 23:24:52 +0800

    init my slides project

---
+++ app/scripts/main.js
@@ -0,0 +1,28 @@
+/*global require*/
+'use strict';
+
+require.config({
+
+	// 非AMD模块的js可使用shim配置导出
+    shim: {
+    	'backbone': {
+    		deps: ['underscore', 'jquery'],
+    		exports: 'Backbone'
+    	}
+    },
+
+	// 根据路径定义别名 alias
+    paths: {
+        jquery: '../bower_components/jquery/dist/jquery',
+        backbone: '../bower_components/backbone/backbone',
+        underscore: '../bower_components/lodash/dist/lodash'
+    }
+});
+
+require([
+    'backbone'
+], function (Backbone) {
+    Backbone.history.start();
+
+    console.log(Backbone);
+});
