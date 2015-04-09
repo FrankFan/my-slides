4316fe49deb1139ad577ff04c55c974de91b616a
FrankFan <fanyong@gmail.com>
2015-04-09 23:50:46 +0800

    add slide animate

--- app/scripts/views/app.js
+++ app/scripts/views/app.js
@@ -24,6 +24,20 @@ function(Backbone, SlidesView, SlidesCollection, MainRouter) {
             App.router = new MainRouter();
             // 开启Backbone路由支持
             Backbone.history.start();
+        },
+
+        events: {
+            'keyup': 'keyUp'
+        },
+
+        keyUp: function(e) {
+            // 37 left
+            // 39 right
+            if( e.keyCode === 37 || e.keyCode === 39 ) {
+                App.Vent.trigger('changeSlide', {
+                    direction: e.keyCode === 39 ? 'next' : 'prev' 
+                });
+            }
         }
     });
 
--- app/scripts/views/slides.js
+++ app/scripts/views/slides.js
@@ -4,14 +4,55 @@ define(['backbone', 'views/slide'], function(Backbone, SlideView) {
         el: $('.slides'),
 
         initialize: function() {
+            this.currentSlideIndex = 1;
+            this.transitionSpeed = 4000;
+
             this.renderAll();
-debugger;
+
             App.Vent.on('init', this.hideAllButFirst, this);
+            App.Vent.on('changeSlide', this.changeSlide, this);
         },
 
         hideAllButFirst: function() {
 
             this.$el.children(':nth-child(n+2)').hide();
+        },
+
+        changeSlide: function(opts) {
+            var newSlide;
+            var slides = this.$el.children();
+            // 
+            if( opts.slideIndex ) {
+                this.currentSlideIndex = ~~opts.slideIndex;
+            } else {
+                this.nextSlide(opts.direction);
+            }
+
+            newSlide = slides.eq(this.currentSlideIndex - 1);
+            
+            this.hideAllButFirst();
+
+            // transition to that slide
+            slides.filter(':visible')
+                .css('position', 'absolute') // TEMPORARY
+                .animate({
+                    top: opts.direction === 'next' ? '100%' : '-100%',
+                    opacity: 'hide'
+                }, this.transitionSpeed, function() {
+                    // slide is gone from view
+                    $(this).css('top', 0);
+
+                    // bring new slide to view                    
+                    newSlide
+                        .animate({
+                            top: 0,
+                            opacity: 'show'
+                        }, this.transitionSpeed);
+                })
+        },
+
+        nextSlide: function() {
+
         },
 
         renderAll: function() {
