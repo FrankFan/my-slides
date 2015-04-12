ce2f78b58ff8047124823166427a2def110ef523
FrankFan <fanyong@gmail.com>
2015-04-12 23:56:21 +0800

    add setFontSize() method in model

--- app/scripts/models/slide.js
+++ app/scripts/models/slide.js
@@ -5,6 +5,24 @@ define(['backbone'], function(Backbone) {
 		defaults: {
 			type: 'note',
 			title: ''
+		},
+
+		initialize: function() {
+			this.setFontSize();
+		},
+
+		setFontSize: function() {
+			var length = this.get('title').length;
+
+			if (length >= 200) {
+				size = 'x-large';
+			} else if (length >= 100) {
+				size = 'large';
+			} else {
+				size = 'normal';
+			}
+
+			this.set('size', size);
 		}
 	});
 
--- app/scripts/slides.js
+++ app/scripts/slides.js
@@ -1,4 +1,6 @@
 window.slides = [
 	{ title: 'My Presitation' },
-	{ title: 'My Name is Frank.' }
+	{ title: 'My Name is Frank.' },
+	{ title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto maxime quas modi, eveniet fugiat. Alias voluptatem cum consectetur nobis quod, excepturi recusandae, itaque facere minima officiis autem illum, perferendis provident!.' },
+
 ];
